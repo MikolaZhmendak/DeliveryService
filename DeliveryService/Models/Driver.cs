01e4b6061f9ab9bf4eff10f082c887e450d79da9
MikolaZhmendak <zhmendakmikola@yahoo.com>
2018-05-17 15:21:18 -0500

    added diver model

---
+++ DeliveryService/Models/Driver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeliveryService.Models
+{
+    public class Driver
+    {
+        [Key]
+        public int DriverId { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public virtual string FirstName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public virtual string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: ###-####-####}")]
+        public long PhoneNumber { get; set; }
+
+
+        [DataType(DataType.PostalCode)]
+        public virtual string ZipCode { get; set; }
+
+
+
+
+    }
+}
