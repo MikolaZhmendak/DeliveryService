f62cd2c41396561ad5c1956a2756385857ae259b
MikolaZhmendak <zhmendakmikola@yahoo.com>
2018-08-21 20:57:38 -0500

    styled log in page

--- DeliveryService/Controllers/AccountController.cs
+++ DeliveryService/Controllers/AccountController.cs
@@ -64,6 +64,54 @@ namespace DeliveryService.Controllers
             return View();
         }
 
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult SmartLogin(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SmartLogin(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+
+
+
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    //      return RedirectToAction(returnUrl);
+                    return RedirectToAction("Home", "Home");
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+
+        }
+
+
+
+
+
         //
         // POST: /Account/Login
         [HttpPost]
